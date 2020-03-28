de9b5a6804a19b85fa3422a37e1be6d46b37aa57
btate712 <btate712@gmail.com>
2020-03-28 13:47:58 -0500

    removed non-class examples

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,35 +1,5 @@
 import UIKit
 
-var str = "Hello, playground"
-
-var counter: Int = 0
-
-repeat {
-    counter += 1
-    print("Hello, world!")
-} while counter < 5
-
-for i in 1...5 {
-    print("\(i): Hello again")
-}
-
-func addNumbers(a: Int, b: Int) -> Int {
-    return a + b
-}
-
-addNumbers(a: 6, b: 7)
-
-var array = [Int](repeating: 0, count: 8)
-for i in 0..<8 {
-    array[i] = i
-}
-print(array)
-
-var months: [Int: String] = [1: "January", 2: "February", 3: "March", 4: "April"]
-for month in months {
-    print(month.value)
-}
-
 class Dog {
     var breed: String
     var age: Int
