24a1f35f6cdf0eb00ba088dcdc485d61f424a203
Bob Tate <btate712@gmail.com>
2020-03-31 16:54:09 -0500

    played with external parameter names

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,55 +1,15 @@
 import UIKit
 
-class Dog {
-    var name: String
-    var breed: String
-    var age: Int
-    
-    init(_ name: String,_ breed: String, _ age: Int) {
-        self.name = name
-        self.breed = breed
-        self.age = age
-    }
-    
-    func bark() {
-        print("Woof.")
-    }
-    
-    func sayHi() {
-        print("Hello, my name is \(self.name). I'm a \(age) year old \(breed).")
-    }
-    
+func heyNeil(thereAre numberOfMoons: Int, moonsSurrounding planetName: String) {
+    print("There are \(numberOfMoons) moons orbiting \(planetName)")
 }
 
-class SuperDog: Dog {
-    func fly() {
-        print("\(self.name) is flying!!!")
-    }
-    
-    override func bark() {
-        print("Superwoof!")
-    }
+heyNeil(thereAre: 67, moonsSurrounding: "Jupiter")
+
+
+func hello(toWho: String) {
+    print("Hello \(toWho).")
 }
 
-var myDogs = [Dog]()
-myDogs.append(Dog("Baylee", "Mutt", 10))
-myDogs.append(Dog("Molly", "Lab", 12))
-myDogs.append(Dog("Missy", "Lab", 100))
-
-print("There are \(myDogs.count) dogs. Dogs - please say hi...")
-for dog in myDogs {
-    dog.sayHi()
-}
-
-var dogsByHome = [String: Dog]()
-dogsByHome["Shorewood"] = myDogs[0]
-dogsByHome["Flossmoor"] = myDogs[1]
-dogsByHome["CCH"] = myDogs[2]
-
-for (location, dog) in dogsByHome {
-    print("\(dog.name) lives in \(location).")
-}
-
-
-
+hello(toWho: "Bob")
 
